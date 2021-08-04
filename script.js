586e38aadda29b4c7c77b761255f3f192584c8e5
pyuser <enes.karakas.e@gmail.com>
2021-08-04 09:16:35 +0300

    lesson 90 starts

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const dateEl = document.getElementById("date-picker");
-
+const countdownEl = document.getElementById("countdownForm");
 
 
 const today = new Date();
 const todayString = today.toISOString().split("T")[0];
 
+function formEventListener(e) {
+    e.preventDefault();
+    const titleValue = e.srcElement[0].value;
+    const dateValue = e.srcElement[1].value;
+}
+
 
 //dateEl minimum date value
 dateEl.setAttribute("min", todayString);
+
+
+
+//event listener on form
+countdownEl.addEventListener("submit", formEventListener);
 
